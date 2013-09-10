3c847e3318058aac66a331b76100f2415de2f7f1
Andreas Ronge <andreas.ronge@gmail.com>
2013-09-10 09:30:00 +0200

    Added jquery

---
+++ app/scripts/location.js
@@ -0,0 +1,19 @@
+// awesome javascript goes here
+
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+  else {
+    $('#demo').html("Geolocation is not supported by this browser.")
+  }
+}
+function showPosition(position) {
+  $('#demo').html("Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude)
+}
+
+//getLocation()
+//$('#demo').html("HEJ")
+//console.log("HOHO2")
